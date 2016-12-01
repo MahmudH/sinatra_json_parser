5a5ccebf957f89dff46a5427ad95a8dc64b6c394
mahmudh <mahmudhssn246@gmail.com>
2016-12-01 22:41:09 +0000

    first commit

---
+++ app.rb
@@ -0,0 +1,17 @@
+ENV["RACK_ENV"] ||= "development"
+
+require 'sinatra/base'
+
+class Bookmark_manager < Sinatra::Base
+
+  get '/' do
+    'JSON PARSING APP'
+  end
+
+  post '/data' do
+
+  end
+
+  # start the server if ruby file executed directly
+  run! if app_file == $0
+end
